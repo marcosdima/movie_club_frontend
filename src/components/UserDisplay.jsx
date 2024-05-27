6c55d84987219e84c68da0d07d9fca367223fff9
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-27 11:46:21 -0300

    new: UserDisplay component

---
+++ src/components/UserDisplay.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux"
+
+const UserDisplay = ({ user }) => {
+    const userLogged = useSelector((state) => state.user);
+    const groups = useSelector((state) => state.groups);
+
+    if (!user) return <div>User not found</div>
+
+    const { username, lastname, name } = user;
+    const groupsName = groups.map(({ name }) => name);
+
+    return (
+        <>
+            <h1>{lastname}, {name} Page</h1>
+            <h3>Username: {username}</h3>
+            <h3>Groups: {groupsName.join(', ')}</h3>
+        </>
+    )
+}
+
+export default UserDisplay;
