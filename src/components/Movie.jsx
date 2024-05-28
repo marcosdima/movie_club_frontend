64c3ceccb01efd67d7c49d526c13ee422d628023
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-27 21:15:51 -0300

    new: activity creation and watched function on movies page

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,3 +1,46 @@
+import { useDispatch, useSelector } from "react-redux";
+import { updateActivity } from "../reducers/groupReducer";
+import { addNewActivity } from "../reducers/groupReducer";
+import genericService from "../services/genericService";
+
+const GroupActivity = ({ movieId }) => {
+    const dispatch = useDispatch();
+    const group = useSelector((state) => state.group);
+
+    if (!group ) return <></>
+
+    const activity = group.history.find((activity) => activity.movie === movieId);
+
+    const markAsWatched = async () => {
+        if (!activity) return;
+        try {
+            const activityWatched = await genericService.update(`activities/${activity.id}/watched`, { groupId: group.id });
+            dispatch(updateActivity(activityWatched));
+        } catch(error) {
+            console.log(error.response.data.error);
+        }
+    }
+
+    const addToGroup = async () => {
+        try {
+            const data = await genericService.create("activities", { movieId, groupId: group.id });
+            dispatch(addNewActivity(data));
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <>
+            {
+                activity
+                ? <button onClick={() => markAsWatched()}>Watched</button>
+                : <button onClick={() => addToGroup()}>Add to group</button>
+            }
+        </>
+    )
+}
+
 const Movie = ({ movie }) => {
     if (!movie) return (
         <div>
@@ -5,7 +48,7 @@ const Movie = ({ movie }) => {
         </div>
     );
     
-    const { title, imageUrl, description, director, genres } = movie;
+    const { title, imageUrl, description, director, genres, id } = movie;
 
     return (
         <div>
@@ -16,6 +59,7 @@ const Movie = ({ movie }) => {
                 Genres: {genres.join(', ')}
             </div>
             <p>Description: {description}</p>
+            <GroupActivity movieId={id}/>
         </div>
     );
 };
