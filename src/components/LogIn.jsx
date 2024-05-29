173660295f3f85c82021ea3b8b06323a7af07d0e
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-29 12:02:04 -0300

    rework: navigate implementation - If you logged in or you are already logged, then sends it to 'movies'.

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,14 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import useField from '../hooks/useField'
 import { login } from '../reducers/userReducer';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const LogIn = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const user = useSelector((state) => state.user);
+
+    // If the user already logged in, then sends it to 'movies'.
+    useEffect(() => {
+        if (user) navigate('/movies');
+    }, [user])
+
     const { reset: resetUsername, ...username } = useField('username');
     const { reset: resetPasword, ...password } = useField('password');
 
     const handleLogin = () => {
         dispatch(login(username.value, password.value));
+        navigate('/movies');
     }
 
     return (
