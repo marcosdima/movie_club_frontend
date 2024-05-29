f65123a4b3acffe366c5eb7944f6bfdf44e0f47d
marcosdima <marcosdimatteo.dev@gmail.com>
2024-05-29 11:56:00 -0300

    new: Button component

---
+++ src/components/tools/Button.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const Button = ({ label, mouseOnColor, color, onClick }) => {
+    const [background, setBackground] = useState(color);
+
+    const style = {
+        backgroundColor: background,
+        width: 'fit-content',
+        padding: 10,
+        fontFamily: 'sans-serif',
+    };
+
+    const changeColor = () => background === color ? setBackground(mouseOnColor) : setBackground(color)
+
+    return (
+        <span
+            style={style}
+            onMouseEnter={() => changeColor()}
+            onMouseLeave={() => changeColor()}
+            onClick={onClick}
+            >
+            {label}
+        </span>
+    )
+}
+
+export default Button
