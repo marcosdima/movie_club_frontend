ea945e887703113ac974d949a912e72a463689ad
marcosdima <marcosdimatteo.dev@gmail.com>
2024-06-03 14:52:45 -0300

    new: icons - modifying groups style

--- src/components/GroupsDisplay.jsx
+++ src/components/GroupsDisplay.jsx
@@ -2,13 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { setGroup } from "../reducers/groupReducer";
 import GroupForm from "./GroupForm";
 import { useDispatch, useSelector } from "react-redux";
+import PersonIcon from '@mui/icons-material/Person';
+import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
 
 const Group = ({ name, members, movies, id, handleClick }) => {
     return (
         <div className="group" onClick={() => handleClick(id)}>
             <h1>{ name }</h1>
-            <p>
-                Members: { members } Movies: { movies }
+            <p className="info">
+                <PersonIcon />{ members } <LocalMoviesIcon /> { movies }
             </p>
         </div>
     );
