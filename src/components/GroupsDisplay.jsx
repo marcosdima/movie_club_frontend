1413091e8a28abe2663d035cba8b6d8558347fa3
marcosdima <marcosdimatteo.dev@gmail.com>
2024-06-20 11:52:01 -0300

    new: delete icon

--- src/components/GroupsDisplay.jsx
+++ src/components/GroupsDisplay.jsx
@@ -3,12 +3,25 @@ import { setGroup } from "../reducers/groupReducer";
 import GroupForm from "./GroupForm";
 import { useDispatch, useSelector } from "react-redux";
 import PersonIcon from '@mui/icons-material/Person';
+import CloseIcon from '@mui/icons-material/Close';
 import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
+import { useState } from "react";
 
 const Group = ({ name, members, movies, id, handleClick }) => {
+    const [mouseOn, setMouseOn] = useState(false);
+    const showIfMouseOn = {
+        display: mouseOn ? '' : 'none'
+    };
     return (
-        <div className="group-display" onClick={() => handleClick(id)}>
-            <h1>{ name }</h1>
+        <div 
+            className="group-display" 
+            onMouseEnter={() => setMouseOn(true)} 
+            onMouseLeave={() => setMouseOn(false)}
+            >
+            <div className="group-display-up">
+                <h1 onClick={() => handleClick(id)} >{ name }</h1>
+                <div style={showIfMouseOn} className='delete-group' onClick={() => console.log('deletete!')}><CloseIcon /></div>
+            </div>
             <p className="info">
                 <PersonIcon />{ members } <LocalMoviesIcon /> { movies }
             </p>
